6f7d41fc6145309ec4d4cef75166150c815f83e7
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2023-01-17 13:03:25 -0300

    feat: :sparkles: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
 const prisma = new PrismaClient
+
+app.register(cors, {
+  origin: ['http://localhost:3000']
+})
 
 app.get('/', async () => {
   const habits = await prisma.habit.findMany({
